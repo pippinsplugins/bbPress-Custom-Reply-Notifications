78782ab54feed8e389cfc5e542742b64d541a88f
Pippin Williamson <pippin@pippinsplugins.com>
2012-10-23 14:43:53 -0500

    Finished settings fields

--- bbp-custom-reply-notifications.php
+++ bbp-custom-reply-notifications.php
@@ -20,104 +20,92 @@ class PW_BBP_Reply_Notifications {
 		add_filter( 'bbp_subscription_mail_message', array( __CLASS__, 'reply_message' ), 10, 4 );
 		add_filter( 'bbp_subscription_mail_title', array( __CLASS__, 'reply_title' ), 10, 3 );
 
-		add_filter( 'bbp_admin_get_settings_sections', array( __CLASS__, 'settings_section' ), 10 );
-		add_filter( 'bbp_admin_get_settings_fields', array( __CLASS__, 'settings_field' ), 10 );
+		add_action( 'admin_init', array( __CLASS__, 'settings' ), 100 );
 
 	}
 
 
 	function reply_message( $message, $reply_id, $topic_id, $user_id ) {
 
+		$reply_content 	= strip_tags( bbp_get_reply_content( $reply_id ) );
+		$reply_url     	= bbp_get_reply_url( $reply_id );
+		$reply_author	= bbp_get_reply_author_display_name( $reply_id );
+
+		$message = str_replace( '{author}', 	$reply_author, 	$message );
+		$message = str_replace( '{content}', 	$reply_content, $message );
+		$message = str_replace( '{url}', 		$reply_url, 	$message );
+
+
+		return $message;
+	}
+
+	function reply_title( $title, $reply_id, $topic_id, $user_id ) {
+
+		$topic_title 	= strip_tags( bbp_get_topic_title( $topic_id ) );
+		$title 			= str_replace( '{title}', $topic_title, $title );
+
+		return $title;
+	}
+
+	function settings() {
+
+		add_settings_section( 'bbp_settings_reply_notices', __( 'Reply Notifications', 'bbpress' ), array( __CLASS__, 'section_callback' ), 'bbpress' );
+
+		add_settings_field( '_bbp_reply_notice_body', __( 'Email Body', 'bbpress' ), array( __CLASS__, 'reply_body_callback' ), 'bbpress', 'bbp_settings_reply_notices' );
+		add_settings_field( '_bbp_reply_notice_title', __( 'Email Subject', 'bbpress' ), array( __CLASS__, 'reply_title_callback' ), 'bbpress', 'bbp_settings_reply_notices' );
+
+		register_setting( 'bbpress', '_bbp_reply_notice_body' );
+		register_setting( 'bbpress', '_bbp_reply_notice_title' );
 
 	}
 
-	function reply_title( $reply_id, $topic_id, $user_id ) {
 
+	function section_callback() {
+	?>
 
+		<p><?php _e( 'Forum settings for new reply notifications', 'bbpress' ); ?></p>
+
+	<?php
 	}
 
-	function settings_section( $sections ) {
 
-		$sections[] = array(
-			'bbp_settings_reply_notices' => array(
-				'title'    => __( 'Reply Notifications', 'bbpress' ),
-				'callback' => 'bbp_admin_setting_callback_reply_notices_section',
-				'page'     => 'bbpress'
-			)
-		);
+	function reply_body_callback() {
 
-		return $sections;
+		$default = '{author} wrote:
+
+	{content}
+				
+	Post Link: {url}
+
+	-----------
+
+	You are receiving this email because you subscribed to a forum topic.
+
+	Login and visit the topic to unsubscribe from these emails.';
+
+
+		$message = bbp_get_form_option( '_bbp_reply_notice_body', $default );
+
+	?>
+
+		<textarea name="_bbp_reply_notice_body" class="large-text" rows="15" id="_bbp_reply_notice_body"><?php echo esc_textarea( $message ); ?></textarea>
+		<label for="_bbp_reply_notice_body"><?php _e( 'Email message sent to topic subscribers when a new reply is posted', 'bbpress' ); ?></label>
+
+	<?php
 	}
 
-	function settings_field( $fields ) {
 
-		$sections['bbp_settings_reply_notices'] = array(
-			'_bbp_reply_notice_body' => array(
-				'title'             => __( 'Email Body', 'bbpress' ),
-				'callback'          => 'pw_bbp_admin_setting_callback_reply_notices_body',
-				'sanitize_callback' => 'sanitize_textarea',
-				'args'              => array()
-			),
-			'_bbp_reply_notice_title' => array(
-				'title'             => __( 'Email Subject', 'bbpress' ),
-				'callback'          => 'pw_bbp_admin_setting_callback_reply_notices_subject',
-				'sanitize_callback' => 'sanitize_textfield',
-				'args'              => array()
-			)
-		);
+	function reply_title_callback() {
 
-		return $fields;
+		$default_title = '[' . get_option( 'blogname' ) . '] {title}';
+	?>
 
-	}
+		<input name="_bbp_reply_notice_title" type="text" id="_bbp_reply_notice_title" value="<?php bbp_form_option( '_bbp_reply_notice_title', $default_title ); ?>" class="regular-text" />
+		<label for="_bbp_reply_notice_title"><?php _e( 'The subject of the notification email', 'bbpress' ); ?></label>
 
-}
+	<?php
+	}	
 
-/**
- * Reply Notice Body Field
- *
- * @since 1.0
- *
- * @uses bbp_form_option() To output the option value
- */
-function pw_bbp_admin_setting_callback_reply_notices_body() {
-
-	$default = '{author} wrote:
-
-{content}
-			
-Post Link: {url}
-
------------
-
-You are receiving this email because you subscribed to a forum topic.
-
-Login and visit the topic to unsubscribe from these emails.';
-
-
-	$message = bbp_get_form_option( '_bbp_reply_notice_body', $default );
-
-?>
-
-	<textarea name="_bbp_reply_notice_body" type="number" min="0" step="1" id="_bbp_reply_notice_body"><?php echo esc_textarea( $message ); ?></textarea>
-	<label for="_bbp_reply_notice_body"><?php _e( 'seconds', 'bbpress' ); ?></label>
-
-<?php
-}
-
-/**
- * Reply Notice Body Subject / Title
- *
- * @since 1.0
- *
- * @uses bbp_form_option() To output the option value
- */
-function pw_bbp_admin_setting_callback_reply_notices_subject() {
-?>
-
-	<input name="_bbp_reply_notice_title" type="number" min="0" step="1" id="_bbp_reply_notice_title" value="<?php bbp_form_option( '_bbp_reply_notice_title', 'New Reply' ); ?>" class="regular-text" />
-	<label for="_bbp_reply_notice_title"><?php _e( 'sbuject', 'bbpress' ); ?></label>
-
-<?php
 }
 
 // load our class
